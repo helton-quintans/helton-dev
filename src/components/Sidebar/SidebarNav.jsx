5e4603beaca99ee7e3f734b322d08de869994c6d
helton-quintans <helton.quit@gmail.com>
2022-12-21 18:59:12 -0300

    feat: sidebar links

--- src/components/Sidebar/SidebarNav.jsx
+++ src/components/Sidebar/SidebarNav.jsx
@@ -1,33 +1,29 @@
-import { Stack } from "@chakra-ui/react";
+import { Stack, Divider } from "@chakra-ui/react";
 import {
   RiContactsLine,
   RiDashboardLine,
   RiGitMergeLine,
   RiInputMethodLine,
+  RiCodeBoxLine,
 } from "react-icons/ri";
+import { VscFilePdf } from "react-icons/vsc";
+import { ImBlog } from "react-icons/im";
+
 import { NavLink } from "./NavLink";
 import { NavSection } from "./NavSection";
 
 export function SidebarNav() {
   return (
-    <Stack spacing="12" align="flex-start">
-      <NavSection title="GERAL">
-        <NavLink icon={RiDashboardLine} href="/dashboard">
-          Dashboard
-        </NavLink>
-        <NavLink icon={RiContactsLine} href="/users">
-          Usuários
-        </NavLink>
-      </NavSection>
-
-      <NavSection title="AUTOMAÇÃO">
-        <NavLink icon={RiInputMethodLine} href="/forms">
-          Formulários
-        </NavLink>
-        <NavLink icon={RiGitMergeLine} href="/automation">
-          Automação
-        </NavLink>
-      </NavSection>
+    <Stack spacing="12" my="8" align="flex-start">
+      <NavLink icon={RiCodeBoxLine} color="gray.700" href="/">
+        Projects
+      </NavLink>
+      <NavLink icon={VscFilePdf} color="gray.700" href="#">
+        Resume
+      </NavLink>
+      <NavLink icon={ImBlog} color="gray.700" href="#">
+        Blog
+      </NavLink>
     </Stack>
   );
 }
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,48 +1,52 @@
 import {
-    Flex,
-    Drawer,
-    Box,
-    Stack,
-    Text,
-    Link,
-    Icon,
-    useBreakpointValue,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    DrawerHeader,
-    DrawerBody,
-  } from "@chakra-ui/react";
-  import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
-  
-  import { SidebarNav } from "./SidebarNav";
-  
-  export function Sidebar() {
-    const { isOpen, onClose } = useSidebarDrawer();
-    // const isDrawerSidebar = useBreakpointValue({
-    //   base: true,
-    //   lg: false,
-    // });
-  
-    // if (isDrawerSidebar) {
-      return (
-        <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
-          <DrawerOverlay>
-            <DrawerContent bg="gray.800" p="4">
-              <DrawerCloseButton mt="10" />
-              <DrawerHeader mt="4">Navigation</DrawerHeader>
-  
-              <DrawerBody>
-                <SidebarNav />
-              </DrawerBody>
-            </DrawerContent>
-          </DrawerOverlay>
-        </Drawer>
-      );
-    // }
-    return (
-      <Box as="aside" w="64" mr="8">
-        <SidebarNav />
-      </Box>
-    );
-  }
+  Flex,
+  Drawer,
+  Box,
+  Stack,
+  Text,
+  Link,
+  Icon,
+  useBreakpointValue,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  Divider,
+} from "@chakra-ui/react";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+
+import { SidebarNav } from "./SidebarNav";
+
+export function Sidebar() {
+  const { isOpen, onClose } = useSidebarDrawer();
+  // const isDrawerSidebar = useBreakpointValue({
+  //   base: true,
+  //   lg: false,
+  // });
+
+  // if (isDrawerSidebar) {
+  return (
+    <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+      <DrawerOverlay>
+        <DrawerContent bg="gray.800" p="4">
+          <DrawerCloseButton mt="10" />
+          <DrawerHeader mt="4">Navigation</DrawerHeader>
+
+          <DrawerBody>
+            <Divider />
+
+            <SidebarNav />
+            <Divider />
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+  );
+  // }
+  return (
+    <Box as="aside" w="64" mr="8">
+      <SidebarNav />
+    </Box>
+  );
+}
