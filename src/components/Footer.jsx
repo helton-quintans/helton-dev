2e1e70b72eab01df5efc8f14c0b80a1999b0468d
helton-quintans <helton.quit@gmail.com>
2023-09-08 09:14:03 -0300

    refactor: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,9 @@
-import { Image } from "@chakra-ui/react";
-
 import {
   Box,
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  Center,
-  Divider,
   Flex,
-  Heading,
-  IconButton,
-  Link,
-  List,
-  ListIcon,
-  ListItem,
-  Stack,
-  StackDivider,
   Text,
 } from "@chakra-ui/react";
-import { FC, memo, PropsWithChildren } from "react";
-import { MdCheckCircle } from "react-icons/md";
-import { SlGraduation } from "react-icons/sl";
 
-import { Header } from "./Header";
-
-import { MdOutlineFileDownload } from "react-icons/md";
 import { Logo } from "./Header/Logo";
 
 export default function Footer() {
@@ -34,26 +12,12 @@ export default function Footer() {
   return (
     <Box py={10}>
       <Flex
-        align={"center"}
+        align="center"
         justify="center"
-        // _before={{
-        //   content: '""',
-        //   borderBottom: "1px solid",
-        //   borderColor: "gray.200",
-        //   flexGrow: 1,
-        //   mr: 8,
-        // }}
-        // _after={{
-        //   content: '""',
-        //   borderBottom: "1px solid",
-        //   borderColor: "gray.200",
-        //   flexGrow: 1,
-        //   ml: 8,
-        // }}
       >
         <Logo />
       </Flex>
-      <Text pt={6} fontSize={"sm"} textAlign={"center"}>
+      <Text pt={6} fontSize={"sm"} textAlign="center">
         Helton Quintãns | All rights reserved | © {currentDate}
       </Text>
     </Box>
